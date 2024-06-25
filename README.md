fee106d0cd5e2029b9069b96d034a9cf5f6e121d
Théophile R. <inkurey22.tr@proton.me>
2024-06-25 10:41:08 +0200

    init: added basics for the repo such as README && the header file with functions list

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # c_matrix
-A matrix management library in C
+### A matrix management library in C
+## Description
+
+This is one of my personal projects.
+It's a library made to manage matrices in C.
+
+Functions are listed in the `include/matrix.h` file and each file is documented.
+## Usage
+
+To use this library:
+- add the `/include/matrix.h` file to your project's include ectory.
+- add the `/matrix_lib` directory to your project's root.
+- Add the matrix files to your project's compilation.
+- And voilà! You can now use the functions in your project.
+
+## Disclaimer
+
+I started programming in october 2023 and all my projects are there to try new things so feel free to address any issues you see or any improvements you think of.
+
+Also, the code here is written to comply with EPITECH's coding style. This is why I use the EPITECH header, some functions are cut in half during loops and if/if are used instead of if/else if (One of the many style errors is : Conditional block with more than 3 branches, or at a nesting level of 3 or more)
---
+++ include/matrix.h
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2024
+** c_matrix
+** File description:
+** matrix.h
+*/
+
+#ifndef C_MATRIX_H
+    #define C_MATRIX_H
+
+    #include <math.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+
+typedef struct matrix {
+    int rows;
+    int cols;
+    double **matrix;
+} matrix_t;
+
+matrix_t *create_matrix(int rows, int cols);
+matrix_t *add_matrix(matrix_t *matrix1, matrix_t *matrix2);
+matrix_t *sub_matrix(matrix_t *matrix1, matrix_t *matrix2);
+matrix_t *multiply_matrix(matrix_t *matrix1, matrix_t *matrix2);
+matrix_t *power_matrix(matrix_t *matrix, int power);
+matrix_t *scalar_matrix(matrix_t *matrix, double scalar);
+matrix_t *transpose_matrix(matrix_t *matrix);
+matrix_t *inverse_matrix(matrix_t *matrix);
+
+int rank(matrix_t *matrix);
+
+double get_value(matrix_t *matrix, int row, int col);
+double determinant_matrix(matrix_t *matrix);
+double trace(matrix_t *matrix);
+
+void destroy_matrix(matrix_t *matrix);
+void print_matrix(matrix_t *matrix);
+void set_indiv_value(matrix_t *matrix, int row, int col, double value);
+void set_matrix(matrix_t *matrix, double **new_matrix);
+
+#endif
